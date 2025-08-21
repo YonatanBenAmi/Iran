62bbccccb01e26f0c2c9e16e7b0c9234f81f44d2
Yonatan Ben Ami <yba6081@gmail.com>
2025-08-21 16:30:56 +0300

    time over

--- app/processor.py
+++ app/processor.py
@@ -1,7 +1,37 @@
-from get_data import GetData
+from fetcher import Fetcher
 import pandas
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
-# class Processor:
 
-    # def 
 
+class Processor:
+
+    fetcher = Fetcher()
+    dict_df = fetcher.get_df().to_dict(orient="index")
+
+    def add_col_rarest(self):
+        for tweet in self.dict_df.keys:
+            print(tweet)
+            self.dict_df["tweet"]["rarest"] = self.get_rarest_word(tweet["Text"])
+        return self.dict_df
+
+    
+    def get_rarest_word(self, text):
+        list_words = text.split()
+        dict_words = {}
+        for word in list_words:
+            if word in dict_words:
+                dict_words[word] += 1
+            else:
+                dict_words[word] = 1
+        return max(dict_words, key=dict_words.get)
+        
+    def get_emotion(self, tweet):
+        nltk.download('vader_lexicon')# Compute sentiment labels
+        score = SentimentIntensityAnalyzer().polarity_scores(tweet)
+        return score
+    
+a = Processor()
+
+print(a.add_col_rarest())
